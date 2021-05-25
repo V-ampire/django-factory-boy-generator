0084bbf93a199b353d4ec30e8f91be37acd24c29
V-ampire <webjob010@gmail.com>
2021-05-25 15:47:45 +1000

    fix imports in utils

--- factory_generator/utils.py
+++ factory_generator/utils.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.core.management.base import CommandError
 
 import configparser
-from enum import Enum
 from factory.django import DjangoModelFactory
 import importlib
 import logging
